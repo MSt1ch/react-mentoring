95da7880a2dc01131548abdfb20cb03739f578f7
Stanislau Matsievsky <st1ch@Stanislaus-MacBook-Pro.local>
2019-03-17 23:40:43 +0300

    02-task# add css modules, change webpack, package.json config

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
-import appStyle from "./appStyle";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Content from './components/Content';
+import css from './index.module.scss';
 
-const greeting = "Welcome aboard,";
-const name = "Stanislau Matsiyeuski";
+const greeting = 'Welcome aboard,';
+const name = 'Stanislau Matsiyeuski';
 
 const App = ({ name }) => (
     <div className="page-wrapper">
-        { Header }
-        <h2 style={ appStyle }>{ name }</h2>
-        <Content/>
+        <Header />
+        <h2 className={css.app}>{name}</h2>
+        <Content />
         <Footer />
     </div>
 );
 
 ReactDOM.render(
-    <App name={`${ greeting } ${ name }.`}/>,
-    document.getElementById("app")
+    <App name={`${greeting} ${name}.`} />,
+    document.getElementById('app')
 );
 
 module.hot.accept();
